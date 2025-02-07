9958b39b039ae5c7e1ef4e1d94eae428b9f61806
ka1ratt3 <hachushka81311@gmail.com>
2025-02-07 12:07:12 +0300

    рHW: Did hw2

--- HomeWorks/hw2.py
+++ HomeWorks/hw2.py
@@ -4,14 +4,10 @@ class Heroes:
         self.hp = hp
 
     def action(self):
-        print(f"{self.name} отрегенился на {self.hp} очков здоровья")
+        print(f"{self.name} готовится к бою!")
 
     def attack(self):
-        print(f'{self.name}s attack')
-
-gerald = Heroes('Gerald', 15)
-gerald.action()
-gerald.attack()
+        print(f"{self.name} атакует врага!")
 
 
 class Archer(Heroes):
@@ -20,5 +16,36 @@ class Archer(Heroes):
         self.arrows = arrows
         self.precision = precision
 
+    def attack(self):
+        if self.arrows > 0:
+            self.arrows -= 1
+            from random import random
+            if random() < self.precision:
+                print(f"{self.name} попадает!")
+            else:
+                print(f"{self.name} промахивается")
+        else:
+            print(f"у {self.name} закончились стрелы")
 
+    def recharge(self):
+        self.arrows += 5
+        print(f"у {self.name} перезарядка Теперь у него {self.arrows} стрел.")
+
+    def status(self):
+        return f"Имя: {self.name}, Здоровье: {self.hp}, Стрелы: {self.arrows}, Точность: {self.precision:.2f}"
+
+
+# Тестирование кода
+if __name__ == "__main__":
+    archer = Archer("batman", 50, 3, 0.5)
+
+    # Проверяем методы
+    print(archer.status())
+    archer.action()
+    archer.attack()
+    archer.attack()
+    archer.attack()
+    archer.attack()
+    archer.recharge()
+    print(archer.status())
 
